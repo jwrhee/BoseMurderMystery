4ddeb40ba3e8ce55440049d97489f49d1845420a
Andrew Garrahan <andrew@computerlunch.com>
2019-06-29 18:54:13 -0400

    Gesture Controller

--- Assets/GestureControl.cs
+++ Assets/GestureControl.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class FramesInputController : MonoBehaviour
+public class GestureControl : MonoBehaviour
 {
    
     public static event Action OnDoubleTap;
---
+++ Assets/Suspect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suspect : MonoBehaviour
+{
+    public enum SuspectName { NONE, WATTS, DRAGUER, CAMMISH, AGATHA, BUTLER };
+    public SuspectName suspectName;
+
+    public AudioSource audio;
+
+    public AudioClip clipSelectedForQuestioning;
+    public AudioClip clipRollOverForQuestioning;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();   
+    }
+}
