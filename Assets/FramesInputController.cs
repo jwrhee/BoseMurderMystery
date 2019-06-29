4fc8d49e7ca08ce82e448efcaed71244d2be5a8d
Andrew Garrahan <andrew@computerlunch.com>
2019-06-29 14:03:52 -0400

    FramesInput Controller and prefab

---
+++ Assets/FramesInputController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FramesInputController : MonoBehaviour
+{
+   
+    public static event Action OnDoubleTap;
+    public static event Action OnHeadNod;
+    public static event Action OnHeadShake;
+
+    void Start()
+    {
+        
+    }
+
+    public void InputDoubleTap()
+    {
+        Debug.Log("Double Tap");
+        if (OnDoubleTap != null)
+        {
+            OnDoubleTap();
+        }
+    }
+
+    public void InputHeadNod()
+    {
+        Debug.Log("Head Nod");
+        if (OnHeadNod != null)
+        {
+            OnHeadNod();
+        }
+    }
+
+    public void InputHeadShake()
+    {
+        Debug.Log("Head Shake");
+        if (OnHeadShake != null)
+        {
+            OnHeadShake();
+        }
+    }
+}
