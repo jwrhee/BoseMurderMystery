305dab678b836fe7ebd1a1b68d36222988682023
Theodore Park <theohpark@gmail.com>
2019-08-02 15:53:09 -0400

    add a delay to gesture controller

--- Assets/GestureControl.cs
+++ Assets/GestureControl.cs
@@ -10,35 +10,55 @@ public class GestureControl : MonoBehaviour
     public static event Action OnHeadNod;
     public static event Action OnHeadShake;
 
+    public float DelayAfterGesture = 2.0f;
+    private float GestureTimer;
+    private bool GestureInputted;
+
     void Start()
     {
-        
+        GestureTimer = DelayAfterGesture;
+    }
+
+    private void FixedUpdate()
+    {
+        if (GestureInputted && GestureTimer > 0.0f)
+        {
+            GestureTimer -= Time.fixedDeltaTime;
+
+        } else
+        {
+            GestureTimer = DelayAfterGesture;
+            GestureInputted = false;
+        }
     }
 
     public void InputDoubleTap()
     {
         //Debug.Log("Double Tap");
-        if (OnDoubleTap != null)
+        if (OnDoubleTap != null && !GestureInputted)
         {
             OnDoubleTap();
+            GestureInputted = true;
         }
     }
 
     public void InputHeadNod()
     {
        // Debug.Log("Head Nod");
-        if (OnHeadNod != null)
+        if (OnHeadNod != null && !GestureInputted)
         {
             OnHeadNod();
+            GestureInputted = true;
         }
     }
 
     public void InputHeadShake()
     {
         //Debug.Log("Head Shake");
-        if (OnHeadShake != null)
+        if (OnHeadShake != null && !GestureInputted)
         {
             OnHeadShake();
+            GestureInputted = true;
         }
     }
 }
